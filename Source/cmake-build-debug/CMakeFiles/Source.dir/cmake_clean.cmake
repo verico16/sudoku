9c93d97bb4c0f431e42240805359ef07c0c2e312
verico16 <dimaborak@gmail.com>
2021-04-07 00:01:24 +0300

    init 1

---
+++ Source/cmake-build-debug/CMakeFiles/Source.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Source.dir/main.c.o"
+  "Source"
+  "Source.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Source.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
